e962360babfa0c66b20a167f51776b0c1000b84d
Konan777 <kovtunsv@gmail.com>
2019-05-13 11:20:52 +0900

    Update chunk.cs

--- DB/chunk.cs
+++ DB/chunk.cs
@@ -1,15 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace Multik
-{
-    public class MTWriter<T>
+    public class MTWriter<T> where T : class
     {
         private List<List<T>> _colls;
-        private CancellationTokenSource _Cancel;
         private string _tableName;
         private string _cleanUpField;
         private string _profileTable;
@@ -23,34 +14,37 @@ namespace Multik
             int chunkSize = data.Count / threads;
             _colls = splitList<T>(data, chunkSize).ToList();
             // Фикс для случая когда data.Count не поделилась ровно на число потоков (10/3 дают 4 _colls)
-            if (_colls.Count() > threads) {
+            if (_colls.Count() > threads)
+            {
                 _colls[_colls.Count - 2].AddRange(_colls[_colls.Count - 1]);
                 _colls.Remove(_colls[_colls.Count - 1]);
             }
         }
         public async void Write(string tableName, string cleanUpField, DateTime startDate, DateTime endDate, string profileTable = null)
         {
-            _Cancel = new CancellationTokenSource();
             InProgress = true;
             var cleanup = true;
             List<Task> Writers = new List<Task>();
             foreach (var coll in _colls)
             {
-                if (_Cancel.IsCancellationRequested) break;
-                Writers.Add(Task.Run(() => { WriteRows(coll, cleanup); } ));
+                CancellationContext.ThrowIfCancellationRequested();
+                Writers.Add(Task.Run(() => { WriteRows(coll, cleanup); }));
                 cleanup = false; // Только один поток выполняет очистку данных
             }
             await Task.WhenAll(Writers);
             InProgress = false;
         }
-        public void Cancel()
-        {
-            _Cancel.Cancel(true);
-        }
         private void WriteRows(List<T> rows, bool cleanup)
         {
-            if (_Cancel.IsCancellationRequested) return;
-            CoreHelper.InsertDailyData<T>(rows, false, cleanup, _startDate, _endDate, _tableName, _cleanUpField, _profileTable);
+            try
+            {
+                CoreHelper.InsertData<T>(rows, false, cleanup, _startDate, _endDate, _tableName, _cleanUpField);
+                //CoreHelper.InsertDailyData<T>(rows, false, cleanup, _startDate, _endDate, _tableName, _cleanUpField, _profileTable);
+            }
+            catch (Exception ex)
+            {
+                throw new EleanorException($"Ошибка при записи данных ", ex);
+            }
         }
         private IEnumerable<List<T>> splitList<T>(List<T> locations, int nSize)
         {
@@ -60,6 +54,3 @@ namespace Multik
             }
         }
     }
-
-
-}
